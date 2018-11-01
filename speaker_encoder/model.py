f2ccd69e3e162547c8ced0913c1096c729ebbfed
fducau <fnd212@nyu.edu>
2018-11-01 18:20:28 +0100

    Rfactoring, name changes and prepare_tensor method added

--- speaker_encoder/model.py
+++ speaker_encoder/model.py
@@ -30,11 +30,11 @@ class WeightClipper(object):
 class SpeakerEncoder(nn.Module):
     def __init__(self, hparams):
         super(SpeakerEncoder, self).__init__()
+        self.device = hparams.device
         self.hidden_size = hparams.lstm_cell_size
         self.depth = hparams.lstm_depth
         self.input_size = hparams.rnn_input_size
         self.dropout = hparams.dropout
-        self.pad_token = hparams.pad_token
         self.N = hparams.N      # Nubmer of individuals per batch
         self.M = hparams.M      # Number of audio samples per individual per batch
 
@@ -49,45 +49,68 @@ class SpeakerEncoder(nn.Module):
         self.w_similarity = torch.tensor(0.5, dtype=torch.float, requires_grad=True)
         self.b_similarity = torch.tensor(0.5, dtype=torch.float, requires_grad=True)
 
-    def rnn_forward(self, sequences_embedded, len_sequences):
+        if self.device == 'gpu':
+            try:
+                self.rnn_network = self.rnn_network.cuda()
+                self.w_similarity = self.w_similarity.cuda()
+                self.b_similarity = self.b_similarity.cuda()
+            except:
+                raise ValueError('device param is gpu but could not upload networks to cuda')
+
+    def prepare_tensors(self, sequences):
+        '''
+        Receives the input sequences in np.array format and casts into
+        tensors. Uploads to GPU if necessary
+        :returns: tensor of sequences in gpu if necessary
+        '''
+        if not isinstance(sequences, (np.ndarray, torch.Tensor)):
+            raise TypeError('Input sequences to the model must be of type np.array')
+
+        if not isinstance(sequences, torch.Tensor):
+            sequences = torch.from_numpy(sequences)
+
+        if self.device == 'gpu':
+            sequences = sequences.cuda()
+
+        return sequences
+
+    def forward(self, sequences):
         '''
         Teacher forcing forward path through RNN for training
+        Assuming that all the sequences have the same length. I.e. no padding.
         '''
+        sequences = self.prepare_tensors(sequences)
 
-        # Order the sequences in descending order or lengths
-        # (required for packing the sequence in torch)
-        seq_sort_order = np.argsort(len_sequences)[::-1]  # Reverse sort order
-        reverse_sort_permutation = np.argsort(seq_sort_order)
+        batch_size = sequences.shape[0]
+        max_seq_len = sequences.shape[1]
+        seq_lengths = [max_seq_len] * batch_size
 
-        len_sequences = len_sequences[seq_sort_order]
-        pack_sequences = nn.utils.rnn.pack_padded_sequence(sequences_embedded,
-                                                           lengths=len_sequences,
+        pack_sequences = nn.utils.rnn.pack_padded_sequence(sequences,
+                                                           lengths=seq_lengths,
                                                            batch_first=True)
 
-        # LSTM Inputs: input, (h_0, c_0)
+        # LSTM Inputs: input, (h_0, c_0). **input** of shape `(seq_len, batch, input_size)`
         # If `(h_0, c_0)` is not provided, both **h_0** and **c_0** default to zero.
         # LSTM Outputs: output, (h_n, c_n)
         # **h_n** of shape `(num_layers * num_directions, batch, hidden_size)`
         # **c_n** (num_layers * num_directions, batch, hidden_size)
         # **output** of shape `(seq_len, batch, num_directions * hidden_size)` (only last(top) layer)
         rnn_out, (h_n, c_n) = self.rnn_network(pack_sequences)
-        c_n = c_n[reverse_sort_permutation]     # Back to the original batch order
+        c_n = c_n[-1]
 
         return c_n
 
-    def forward(self, sequences, len_sequences):
+    def compute_similarity(self, sequences):
         '''
-        Training forward path - teacher forcing.
         :param sequences: tensor of label sequences [batch_size]x[max_sequence_len]
                           sorted by length.
-        :param len_sequences: np.array, length of sequences, in sorted order.
-        :returns: output logits
+        :returns: similarity matrix to compute loss.
         '''
         batch_size = sequences.shape[0]
         max_seq_len = sequences.shape[1]
 
         # ###         RNN Step        # ###
-        E = self.rnn_forward(sequences, len_sequences)     # [batch_size] x [embedding_size]
+        E = self.forward(sequences)     # [batch_size] x [embedding_size]
         E = F.normalize(E, p=2, dim=1)      # TODO: Check equation v = \frac{v}{\max(\lVert v \rVert_p, \epsilon)}
 
         # Compute the centroid embedding for each of the speakers.
@@ -134,8 +157,9 @@ class SpeakerEncoder(nn.Module):
         # S = torch.cat(S, dim=1)     # [batch_size]x[N]
         return S
 
-    def train_batch(self, sequences, len_sequences):
-        S = self.forward(sequences, len_sequences)
+    def train_batch(self, sequences):
+
+        S = self.compute_similarity(sequences)
         # TODO: 
         #   - LOSS COMPUTATION
         #   - BACKWARD
